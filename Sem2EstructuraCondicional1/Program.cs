d58128c150f00402c872117d0da5937a79bc9bc0
OMAR <OMAR@OMAR-GG>
2023-08-27 14:03:48 -0500

    Agregar archivos de proyecto.

---
+++ Sem2EstructuraCondicional1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese los 3 numeros\n");
+int num1=int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+if (num1 > num2 && num1 > num3)
+{
+    Console.WriteLine(num1 + " es mayor ");
+}
+else if (num2 > num1 && num2 > num3)
+{
+    Console.WriteLine(num2 + " es mayor");
+
+}
+else {
+
+    Console.WriteLine(num3 + " es mayor");
+}
